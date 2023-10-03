0a328edde64a2eb00aa95a64d9c6eac6e2c6bef7
Eric Matheus <ericmangabeira@gmail.com>
2023-10-02 22:54:15 -0300

    Novas atualizações

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -29,5 +34,22 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		System.out.println("=========================================");
 		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dadosSerie);
+		System.out.println("=========================================");
+		apiUrl = "http://www.omdbapi.com/?t=gilmore+girls&season=1&episode=1&apikey=bc3d9b9c";
+		json = consumoAPI.obterDados(apiUrl);
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+		System.out.println("=========================================");
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i < dadosSerie.totalTemporadas(); i++){
+			apiUrl = "http://www.omdbapi.com/?t=gilmore+girls&season="+i+"&apikey=bc3d9b9c";
+			json = consumoAPI.obterDados(apiUrl);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 }
